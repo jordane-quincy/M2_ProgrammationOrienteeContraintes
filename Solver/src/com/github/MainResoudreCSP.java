e65267bfa65134829d02e233967d7bab8987c812
jordane-quincy <jordanequincy@gmail.com>
2016-12-30 12:35:11 +0100

    Refacto MainResoudreCSP

--- Solver/src/com/github/MainResoudreCSP.java
+++ Solver/src/com/github/MainResoudreCSP.java
@@ -1,53 +1,74 @@
 package com.github;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainResoudreCSP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int nbVariables = 3;
 		int tailleMaxDomaine = 4;
 		int densite = 80;
 		int durete = 50;
 		int connectivite = 3;
 		CSP csp = new CSP(nbVariables, tailleMaxDomaine, densite, durete, connectivite);
-		System.out.println("On essaye de résoudre le csp suivant :");
+		System.out.println("On essaye de rï¿½soudre le csp suivant :");
 		System.out.println(csp);
-		
-		List<Solution> allSolutionsByBT = Solver.resolveByBacktracking(csp);
-		if (allSolutionsByBT.size() == 0) {
-			System.out.println("Pas de solution pour ce csp");
+
+		Map<String, List<Solution>> map = new HashMap<String, List<Solution>>();
+		map.put("Back Tracking", Solver.resolveByBacktracking(csp));
+		map.put("Back Jumping", Solver.resolveByBackJumping(csp));
+		map.put("Forward Checking", Solver.resolveByForwardChecking(csp));
+
+		for (String nomMethodeResolution : map.keySet()) {
+			List<Solution> allSolutions = map.get(nomMethodeResolution);
+			if (allSolutions.size() == 0) {
+				System.out.println("Pas de solution pour ce csp");
+			} else {
+				System.out.println(
+						"\nSolution par " + nomMethodeResolution + " : ( " + allSolutions.size() + " solutions)\n");
+			}
+			for (Solution solution : allSolutions) {
+				System.out.println(solution);
+			}
 		}
-		else {
-			System.out.println("\nSolution par BT : ( "+ allSolutionsByBT.size() + " solutions)\n");
-		}
-		for (int i = 0; i < allSolutionsByBT.size(); i++) {
-			System.out.println(allSolutionsByBT.get(i));
-		}
-		
-		List<Solution> allSolutionsByBJ = Solver.resolveByBackJumping(csp);
-		if (allSolutionsByBJ.size() == 0) {
-			System.out.println("Pas de solution pour ce csp");
-		}
-		else {
-			System.out.println("\nSolution par BJ : (" + allSolutionsByBJ.size() + " solutions)\n");
-		}
-		for (int i = 0; i < allSolutionsByBJ.size(); i++) {
-			System.out.println(allSolutionsByBJ.get(i));
-		}
-		
-		List<Solution> allSolutionsByFC = Solver.resolveByForwardChecking(csp);
-		if (allSolutionsByFC.size() == 0) {
-			System.out.println("Pas de solution pour ce csp");
-		}
-		else {
-			System.out.println("\nSolution par FC : (" + allSolutionsByFC.size() + " solutions)\n");
-		}
-		for (int i = 0; i < allSolutionsByFC.size(); i++) {
-			System.out.println(allSolutionsByFC.get(i));
-		}		
-		
+		// System.out.println("*********************");
+		//
+		// List<Solution> allSolutionsByBT = Solver.resolveByBacktracking(csp);
+		// if (allSolutionsByBT.size() == 0) {
+		// System.out.println("Pas de solution pour ce csp");
+		// } else {
+		// System.out.println("\nSolution par BT : ( " + allSolutionsByBT.size()
+		// + " solutions)\n");
+		// }
+		// for (int i = 0; i < allSolutionsByBT.size(); i++) {
+		// System.out.println(allSolutionsByBT.get(i));
+		// }
+		//
+		// List<Solution> allSolutionsByBJ = Solver.resolveByBackJumping(csp);
+		// if (allSolutionsByBJ.size() == 0) {
+		// System.out.println("Pas de solution pour ce csp");
+		// } else {
+		// System.out.println("\nSolution par BJ : (" + allSolutionsByBJ.size()
+		// + " solutions)\n");
+		// }
+		// for (int i = 0; i < allSolutionsByBJ.size(); i++) {
+		// System.out.println(allSolutionsByBJ.get(i));
+		// }
+		//
+		// List<Solution> allSolutionsByFC =
+		// Solver.resolveByForwardChecking(csp);
+		// if (allSolutionsByFC.size() == 0) {
+		// System.out.println("Pas de solution pour ce csp");
+		// } else {
+		// System.out.println("\nSolution par FC : (" + allSolutionsByFC.size()
+		// + " solutions)\n");
+		// }
+		// for (int i = 0; i < allSolutionsByFC.size(); i++) {
+		// System.out.println(allSolutionsByFC.get(i));
+		// }
+
 	}
 
 }
